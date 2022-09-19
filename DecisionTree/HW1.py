ecfcc09de4bc7c1af890195f04fc1ad04e6eb351
Xin Yao <90070559+XYao2021@users.noreply.github.com>
2022-09-18 23:33:44 -0600

    Add files via upload

--- DecisionTree/HW1.py
+++ DecisionTree/HW1.py
@@ -4,154 +4,71 @@ from functions import *
 
 
 "label in the last column of each dataset"
-train_dataset = open_file('car/train.csv')
-test_dataset = open_file('car/test.csv')
+# train_dataset = open_file('car/train.csv')
+# test_dataset = open_file('car/test.csv')
 
-tree_depth = 6
-features, counts = get_features_and_num(dataset=train_dataset)
-labels = features[-1]
-org_features = features.copy()
-# print(labels)
-train_dataset = [train_dataset]
-counts = [counts]
-features = [features]
-# org_features = features.copy()
-print('org_features: ', org_features)
-# print('counts: ', counts[0][:-1])
-# print(train_dataset[0][0])
-TREE = []
+train_dataset = open_file('bank/train.csv')
+test_dataset = open_file('bank/test.csv')
 
-for i in range(tree_depth):
-    new_counts = []
-    new_f = []
-    new_d = []
-    Tree = [[] for _ in range(len(train_dataset))]
-    for j in range(len(train_dataset)):
-        # print(i, j, len(train_dataset), len(train_dataset[j]), train_dataset[j])
-        # print(i, j, 'feature: ', len(features[j]), len(features), features[j])
-        # print(i, j, 'counts: ', len(counts[j]), len(counts), counts[j])
-        # print(i, j, 'data_set: ', len(train_dataset), len(train_dataset[j]), train_dataset[j])
-        # print(i, j, 'counts[j][-1]: ', counts[j][-1])
-        # print(i, j, len([i for i in counts[j][-1] if i != 0]))
-        # print(j, len(train_dataset))
-        if len([i for i in counts[j][-1] if i != 0]) != 1:
-            E = Entropy(counts[j][-1])
-            # print(j, 'Entropy: ', E)
-            Gain = information_gain(features[j], counts[j], train_dataset[j], labels, E, org_features)
-            # print('\n', j, 'gain: ', Gain)
-            # print(new_features[Gain.index(max(Gain))],  Gain.index(max(Gain)), new_features.index(new_features[Gain.index(max(Gain))]))
-            new = get_new_data(train_dataset[j], features[j][Gain.index(max(Gain))], org_features.index(features[j][Gain.index(max(Gain))]))
-            # new_dataset.append(new)
-            # print(j, 'new data: ', len(new), new)
-            new_features = features[j].copy()
-            Tree[j] = features[j][Gain.index(max(Gain))]
-            print(j, 'feature remove: ', features[j][Gain.index(max(Gain))])
-            print(j, 'Tree for now: ', Tree, '\n')
-            new_features.remove(features[j][Gain.index(max(Gain))])
-            # print(j, 'new_features: ', new_features)
-            n_counts = []
-            for n in new:
-                # print(len(n), n)
-                b = []
-                # b.append(get_num_labels(n, labels))
-                for nf in new_features[:-1]:
-                    # print(nf, org_features.index(nf))
-                    b.append(get_num_feature(n, nf, org_features.index(nf)))
-                b.append(get_num_feature(n, labels, -1))
-                n_counts.append(b)
-            print(j, 'n_counts: ', len(n_counts), n_counts, '\n')
-            # new_labels = n_counts[-1]
-            # print('new_labels: ', new_labels)
-            new_d += new
-            new_counts += n_counts
-            # new_f.append(new_features)
-            new_f += [new_features for _ in range(len(new))]
-        else:
-            # new_counts.append([])
-            # new_f.append([])
-            Tree[j] = labels[counts[j][-1].index(sum(counts[j][-1]))]
-            print(j, '[END] Label for this branch is ', labels[counts[j][-1].index(sum(counts[j][-1]))], '\n')
-    # print(len(new))
-    train_dataset = new_d
-    # counts = [i for i in new_counts if i != []]
-    counts = new_counts
-    features = new_f
-    TREE.append(Tree)
-    print(i, 'Tree for this round: ', Tree)
-    print(i, len(Tree))
-    # print('for', i+1, 'NEW features: ', len(features), features, len(features))
-    # print('for', i+1, 'NEW data: ', len(train_dataset), train_dataset)
-    # print('for', i+1, 'NEW counts: ', len(counts), counts, 'for next round')
-    print('[END] level ', i, '\n')
+tree_depth = 5
 
-for t in range(len(TREE)):
-    print('Length for level ', t, len(TREE[t]))
-    print('Tree for level ', t, TREE[t], '\n')
+dic = training(train_dataset, tree_depth)
 
-#     else:
-#         new_counts = []
-#         for n in new:
-#             b = []
-#             # b.append(get_num_labels(n, labels))
-#             for nf in new_features:
-#                 b.append(get_num_feature(n, nf, features.index(nf)))
-#             # print(b)
-#             new_counts.append(b)
-#         print('new_counts: ', new_counts)
-#         for j in range(len(new_counts)):
-#             # print(j, new_counts[j])
-#             # print(j, new_features)
-#             Es = Entropy(new_counts[j][-1])
-#             # print(Es, '\n')
-#             if Es != 0:
-#                 gain = information_gain(new_features, new_counts[j], new[j], labels, Es)
-#                 print(j, gain, new_features[gain.index(max(gain))])
-#             else:
-#                 print('label end here')
+# test_data = test_dataset[435]
+# print(test_data)
+# B = []
+# A = []
+a = testing(train_dataset, dic)
+b = testing(test_dataset, dic)
+print(a)
+print(b)
+# print(dic[A[B.index(max(B))]][0])
 
-# E = Entropy(counts[-1])
-# # print(E)
-# # print(features.index(labels), len(features))
-# Gain = information_gain(features, counts, train_dataset, labels, E)
-# new = get_new_data(train_dataset, features[Gain.index(max(Gain))], Gain.index(max(Gain)))
-# Tree.append(features[Gain.index(max(Gain))])
-# # print(len(new), len(new[0]), len(new[1]), len(new[2]))
-# # print(new)
-# print(Tree)
-# # print(Gain, features[Gain.index(max(Gain))])
-#
-# new_features = features.copy()
-# new_features.remove(features[Gain.index(max(Gain))])
-# print(new_features)
-# # print(features.index(labels))
-# new_counts = []
-# for n in new:
-#     b = []
-#     # b.append(get_num_labels(n, labels))
-#     for nf in new_features:
-#         b.append(get_num_feature(n, nf, features.index(nf)))
-#     # print(b)
-#     new_counts.append(b)
-# print('new_counts: ', new_counts)
-# new1 = []
-# for j in range(len(new_counts)):
-#     # print(j, new_counts[j])
-#     # print(j, new_features)
-#     Es = Entropy(new_counts[j][-1])
-#     # print(Es, '\n')
-#     if Es != 0:
-#         gain = information_gain(new_features, new_counts[j], new[j], labels, Es)
-#         # print(j, gain, new_features[gain.index(max(gain))])
-#         Tree.append(new_features[gain.index(max(gain))])
-#         new1.append(get_new_data(new[j], new_features[gain.index(max(gain))], features.index(new_features[gain.index(max(gain))])))
-#     else:
-#         print('branch', j, 'end here, generate label')
-#         Tree.append('end')
-#         new1.append([])
-# print(Tree)
-# print(len(new1), len(new1[0]), len(new1[0][0]), len(new1[0][1]), len(new1[0][2]))
-# a = []
-# for f in range(len(new_features[:-1])):
-#     a.append(get_num_feature(new, new_features[f], features.index(new_features[f])))
-# print(a)
-# new_gain = information_gain(features, counts, train_dataset, labels, E)
+# label = dic[A.index(B.index(max(B)))][0]
+# print(test_data)
+# print(dic[A[-1]][0])
+# print(len(dic))
+# print(test_dataset[6][:-1])
+# A = []
+
+# test_data = test_dataset[7]
+# print(test_data)
+# for key in dic.keys():
+#     pred, order = dic[key]
+#     print(order)
+#     a = [key for i in order if test_data[i] == list(key)[i]]
+#     print(test_data)
+#     print(a, '\n')
+
+# correct = 0
+# for data in test_dataset:
+#     test = data[:-1]
+#     label = data[-1]
+#     A = []
+#     for key in dic.keys():
+#         pred, order = dic[key]
+#         a = [key for i in order if test[i] == list(key)[i]]
+#         # print(a, '\n')
+#         A.append(a)
+#     A = [i for i in A if i != []]
+#     print(len(data), data)
+#     print(test_dataset.index(data), A, '\n')
+# #     pred = dic[list(dic.keys())[A.index(max(A))]]
+#     if pred == label:
+#         correct += 1
+#     B = []
+#     m = max(A)
+#     for i in range(len(A)):
+#         if A[i] == m:
+#             B.append(i)
+#     print(len(B), B, '\n')
+    # print(pred, label)
+# print(correct/len(test_dataset))
+
+# for key in dic.keys():
+#     print(key)
+# for test_data in test_dataset:
+#     label = test_data[-1]
+#     features = test_data[:-1]
+#     print(features)
+#     print(label, '\n')
--- DecisionTree/functions.py
+++ DecisionTree/functions.py
@@ -55,6 +55,7 @@ def get_new_data(dataset, target, id):
         for tar in target:
             if data[id] == tar:
                 new_dataset[target.index(tar)].append(data)
+    # return new_dataset
     return [i for i in new_dataset if i != []]
 
 def Entropy(dataset):
@@ -69,33 +70,117 @@ def Entropy(dataset):
             entropy += -P[i]*np.log2(P[i])
     return entropy
 
+def Majority_Error(dataset):  # Only for binary dataset
+    return min(dataset)/sum(dataset)
+
+def Gini_index(dataset):
+    P = []
+    for num in dataset:
+        P.append(num / sum(dataset))
+    Gini = 1
+    for i in range(len(P)):
+        Gini -= P[i]*P[i]
+    return Gini
+
 def information_gain(new_features, counts, dataset, labels, E, features):  # E (Entropy) is the total Entropy for dataset S
     Gain = []
-    # print(features)
     for i in range(len(new_features[:-1])):
-        # print(i, 'new_features: ', new_features)
-        # print(i, 'new_features[i]: ', new_features[i])
-        # print(i, 'dataset: ', dataset)
         f_data = get_new_data(dataset, new_features[i], features.index(new_features[i]))
-        # print(i, len(f_data), f_data, '\n')
-        # print(i, 'fd', new_features[i])
+        L = [len(item) for item in f_data]
         En = [0 for i in range(len(f_data))]
-        for sub_data in f_data:
-            a = get_num_feature(sub_data, labels, -1)
-            # print(i, f_data.index(sub_data), 'a', a)
-            En[f_data.index(sub_data)] = Entropy(a)
         P = []
-        # print(i, 'counts[i]', counts[i])
-        for num in counts[i]:
-            # print(i, num, '\n')
-            P.append(num / sum(counts[i]))
-        # print('Compute Gain: ', En, P, len(f_data))
-        if len(En) != len(P):
-            P = [i for i in P if i != 0]
+        for sub_data in f_data:
+            P.append(len(sub_data) / sum(L))
+            if len(sub_data) != 0:
+                a = get_num_feature(sub_data, labels, -1)
+
+                En[f_data.index(sub_data)] = Entropy(a)
+                # En[f_data.index(sub_data)] = Gini_index(a)
+                # En[f_data.index(sub_data)] = Majority_Error(a)
         gain = 0
+        # print(P)
         for j in range(len(P)):
             gain += P[j] * En[j]
         Gain.append(E - gain)
     return Gain
 
+def training(train_dataset, tree_depth):
+    features, counts = get_features_and_num(dataset=train_dataset)
+    print('The dataset has ', len(features), 'features: ', features)
+    labels = features[-1]
+    org_features = features.copy()
+    train_dataset = [train_dataset]
+    counts = [counts]
+    org_d = [[0 for _ in range(len(features)-1)]]
+    features = [features]
+    dic = {}
+    DIC = {}
+    TREE = [[]]
+    for i in range(tree_depth):
+        new_counts = []
+        new_d = []
+        new_f = []
+        new_dict_label = []
+        new_Tree = []
+        for j in range(len(train_dataset)):
+            E = Entropy(counts[j][-1])
+            # E = Gini_index(counts[j][-1])
+            # E = Majority_Error(counts[j][-1])
 
+            Gain = information_gain(features[j], counts[j], train_dataset[j], labels, E, org_features)
+            new = get_new_data(train_dataset[j], features[j][Gain.index(max(Gain))], org_features.index(features[j][Gain.index(max(Gain))]))
+            new_features = features[j].copy()
+            new_features.remove(features[j][Gain.index(max(Gain))])
+            n_counts = []
+            NEW = new.copy()
+            n_f = []
+            Tree = TREE[j].copy()
+            Tree.append(org_features.index(features[j][Gain.index(max(Gain))]))
+            for n in new:
+                dic_label = org_d[j].copy()
+                sub_labels = get_num_feature(n, labels, -1)
+                dic_label[org_features.index(features[j][Gain.index(max(Gain))])] = features[j][Gain.index(max(Gain))][new.index(n)]
+                if len([item for item in sub_labels if item != 0]) == 1:
+                    dic[tuple(dic_label)] = (labels[sub_labels.index(sum([i for i in sub_labels if i != 0]))], Tree)
+                    # DIC[tuple(dic_label)] = Tree
+                    NEW.remove(n)
+                else:
+                    dic[tuple(dic_label)] = (labels[sub_labels.index(max(sub_labels))], Tree)
+                    b = []
+                    for nf in new_features[:-1]:
+                        b.append(get_num_feature(n, nf, org_features.index(nf)))
+                    b.append(get_num_feature(n, labels, -1))
+                    n_counts.append(b)
+                    n_f.append(dic_label)
+            Tree = [Tree for _ in range(len(NEW))]
+            new_Tree += Tree
+            new_d += NEW
+            new_counts += n_counts
+            # new_f.append(new_features)
+            new_f += [new_features for _ in range(len(new))]
+            new_dict_label += n_f
+        train_dataset = new_d
+        counts = new_counts
+        features = new_f
+        org_d = new_dict_label
+        TREE = new_Tree
+    print('[TRAINING COMPLETE]')
+    return dic
+
+def testing(test_dataset, dic):
+    correct = 0
+    for test_data in test_dataset:
+        A = []
+        label = test_data[-1]
+        for item in dic.keys():
+            a = [0 for _ in range(len(item))]
+            for i in list(dic[item])[1]:
+                if test_data[i] == list(item)[i]:
+                    a[i] = list(item)[i]
+            if tuple(a) == item:
+                A.append(tuple(a))
+        if len(A) != 0:
+            pred = dic[A[-1]][0]
+            if pred == label:
+                correct += 1
+    return correct / len(test_dataset)
